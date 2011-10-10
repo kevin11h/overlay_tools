24c5bbb74bbf609679914b2c70e3c13c6837354e
Maksim A. Boyko <maksim.a.boyko@gmail.com>
2011-10-10 15:18:15 +0400

    Fixed API.

--- overlay_tools.py
+++ overlay_tools.py
@@ -9,19 +9,28 @@ FFMPEG_CMD = "/usr/bin/ffmpeg"
 CONVER_CMD = "/usr/bin/convert"
 MENCODER_CMD = "/usr/bin/mencoder"
 
-def create_video(filename, image):
+OVERLAY_CENTER = "W/2-w/2:H/2-h2"
+OVERLAY_TOP_LEFT = "0:H-h"
+OVERLAY_TOP_RIGHT = "W-h:H-h"
+OVERLAY_BOTTOM_LEFT = "0:0" 
+OVERLAY_BOTTOM_RIGHT = "W-h:0"
+
+def create_video(filename, image, params=None):
     "Create video from animated gif"
     pass
 
-def video_length(filename):
-    "Get video lenght"
-    pass
+def video_params(filename):
+    "Get video length, height and width"
+    length = 0
+    height = 0
+    width = 0
+    return (length, height, width)
 
-def convert_video(filename, container):
+def convert_video(filename, extension="mp4", params=None):
     "Convert video"
     pass
 
-def overlay_video(filename, overlay):
+def overlay_video(filename, overlay, params=None):
     "Overlay video or image"
     pass
 
