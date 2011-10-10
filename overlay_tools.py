c1ce913bd6cef427227a37d903f58ab2ee2b3974
Maksim A. Boyko <maksim.a.boyko@gmail.com>
2011-10-10 16:17:48 +0400

    Added implementation of convert_video function

--- overlay_tools.py
+++ overlay_tools.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import subprocess
+import subprocess as sp
 import sys
 import os
 import optparse
@@ -9,11 +9,11 @@ FFMPEG_CMD = "/usr/bin/ffmpeg"
 CONVER_CMD = "/usr/bin/convert"
 MENCODER_CMD = "/usr/bin/mencoder"
 
-OVERLAY_CENTER = "W/2-w/2:H/2-h2"
-OVERLAY_TOP_LEFT = "0:H-h"
-OVERLAY_TOP_RIGHT = "W-h:H-h"
-OVERLAY_BOTTOM_LEFT = "0:0" 
-OVERLAY_BOTTOM_RIGHT = "W-h:0"
+OVERLAY_CENTER = "(W-w)/2:(H-h)/2"
+OVERLAY_BOTTOM_LEFT = "0:H-h"
+OVERLAY_BOTTOM_RIGHT = "W-h:H-h"
+OVERLAY_TOP_LEFT = "0:0" 
+OVERLAY_TOP_RIGHT = "W-h:0"
 
 def create_video(filename, image, params=None):
     "Create video from animated gif"
@@ -28,13 +28,29 @@ def video_params(filename):
 
 def convert_video(filename, extension="mp4", params=None):
     "Convert video"
-    pass
+
+    if not os.path.exists(filename):
+        raise Exception("No such file %s" % filename)
+
+    path = "%s.%s" % (os.path.splitext(filename)[0], extension)
+    if params:
+        cmd = "%s -y -i %s %s %s" % (FFMPEG_CMD, filename, params, path)
+    else:
+        cmd = "%s -y -i %s %s" % (FFMPEG_CMD, filename, path)
+
+    p = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+    (stdoutdata, stderrdata) = p.communicate()
+    if p.returncode:
+        raise Exception("Return code is not null")
+
+    return path
 
 def overlay_video(filename, overlay, params=None):
     "Overlay video or image"
     pass
 
 def main(argv):
+    convert_video(sys.argv[1])
     print argv
 
 if __name__ == "__main__":
