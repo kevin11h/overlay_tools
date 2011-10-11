f19fddf6cd8c7f5e82a7a1c66299efc519d9db3f
Maksim A. Boyko <maksim.a.boyko@gmail.com>
2011-10-12 00:43:14 +0400

    Added video_params implementation and command line arguments

--- overlay_tools.py
+++ overlay_tools.py
@@ -3,10 +3,6 @@
 import subprocess as sp
 import sys
 import os
-import re
-import glob
-import tempfile
-import optparse
 
 FFMPEG_CMD = "/usr/bin/ffmpeg"
 FFPROBE_CMD = "/usr/bin/ffprobe"
@@ -21,6 +17,8 @@ OVERLAY_TOP_RIGHT = "W-h:0"
 
 def create_video(image, video, length, framerate=5, params=''):
     "Create video from animated gif"
+    import glob
+    import tempfile
 
     if not os.path.isabs(image):
         image = os.path.abspath(image)
@@ -73,11 +71,36 @@ def create_video(image, video, length, framerate=5, params=''):
     os.rmdir(tmpdir)
      
 def video_params(video):
-    "Get video length, height and width"
+    "Get video length, width and height"
+    import re
+
     length = 0
     height = 0
     width = 0
-    return (length, height, width)
+
+    cmd = "%s %s" % (FFPROBE_CMD, video)
+    p = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+    (stdoutdata, stderrdata) = p.communicate()
+    if p.returncode:
+        raise Exception("Return code is not null")
+
+    duration_regexp = re.compile(".*Duration:\s(?P<hours>[0-9.]+):(?P<minutes>[0-9.]+):(?P<seconds>[0-9.]+),.*")
+    width_height_regexp = re.compile(".*Stream.*,\s(?P<width>\d+)x(?P<height>\d+),.*")
+
+    for line in stderrdata.split('\n'):
+        match = duration_regexp.match(line)
+        if not length and match:
+            seconds = int(round(float(match.groupdict()["seconds"]))) 
+            minutes = int(round(float(match.groupdict()["minutes"])))
+            hours = int(round(float(match.groupdict()["hours"])))
+            length = seconds + 60 * minutes + 3600 * hours
+            
+        match = width_height_regexp.match(line)
+        if not height and not width and match:    
+            width = match.groupdict()["width"]
+            height = match.groupdict()["height"]
+
+    return (length, width, height)
 
 def convert_video(video, extension="mp4", params=''):
     "Convert video"
@@ -112,8 +135,78 @@ def overlay_video(video, overlay, new_video, overlay_params=OVERLAY_CENTER, vide
         raise Exception("Return code is not null")
 
 def main(argv):
-    create_video(sys.argv[1], sys.argv[2], int(sys.argv[3]), int(sys.argv[4]))
-    print argv
+    from optparse import OptionParser
+    
+    usage = "usage: %prog [options] <input_video>"
+    parser = OptionParser(usage)
+    
+    parser.add_option("--overlay-center",
+        action="store_true",
+        dest="overlay_center",
+        default=False,
+        help="overlay at center of input video") 
+
+    parser.add_option("--overlay-bottom-left",
+        action="store_true",
+        dest="overlay_bottom_left",
+        default=False,
+        help="overlay at bottom left of input video") 
+    
+    parser.add_option("--overlay-bottom-right",
+        action="store_true",
+        dest="overlay_bottom_right",
+        default=False,
+        help="overlay at bottom right of input video") 
+
+    parser.add_option("--overlay-top-left",
+        action="store_true",
+        dest="overlay_top_left",
+        default=False,
+        help="overlay at top left of input video") 
+    
+    parser.add_option("--overlay-top-right",
+        action="store_true",
+        dest="overlay_top_right",
+        default=False,
+        help="overlay at top right of input video") 
+
+    parser.add_option("-i", "--animated-image",
+        action="store",
+        type="string",
+        dest="animated_image",
+        metavar="IMAGE",
+        help="set overlay animated IMAGE")
+    
+    parser.add_option("-o", "--output-video",
+        action="store",
+        type="string",
+        dest="output_video",
+        metavar="FILE",
+        help="set output video filename")
+
+    (options, args) = parser.parse_args()
+    
+    if options.overlay_center:
+        overlay_place = OVERLAY_CENTER
+    elif options.overlay_bottom_left:
+        overlay_place = OVERLAY_BOTTOM_LEFT
+    elif options.overlay_bottom_right:
+        overlay_place = OVERLAY_BOTTOM_RIGHT
+    elif options.overlay_top_left:
+        overlay_place = OVERLAY_TOP_LEFT
+    elif options.overlay_top_left:
+        overlay_place = OVERLAY_TOP_RIGHT
+    
+    animated_image = options.animated_image   
+    if not os.path.isabs(animated_image):
+        animated_image = os.path.abspath(animated_image)
+    
+    video = args[0]
+    if not os.path.isabs(video):
+        video = os.path.abspath(video)
+    
+    
+    new_video = os.path.abspath()
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
