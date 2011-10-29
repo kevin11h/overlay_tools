78bfdf2451d47ead75ef0fec5288536ffb421520
Maksim A. Boyko <maksim.a.boyko@gmail.com>
2011-10-29 22:14:08 +0400

    Fixed bug in create_overlay_video function. Added functions split_video and merge_video.

--- overlay_tools.py
+++ overlay_tools.py
@@ -4,17 +4,17 @@ import subprocess as sp
 import sys
 import os
 
-FFMPEG_CMD = "/usr/bin/ffmpeg"
-FFPROBE_CMD = "/usr/bin/ffprobe"
-CONVERT_CMD = "/usr/bin/convert"
-IDENTIFY_CMD = "/usr/bin/identify"
-MENCODER_CMD = "/usr/bin/mencoder"
+FFMPEG_CMD = '/usr/bin/ffmpeg'
+FFPROBE_CMD = '/usr/bin/ffprobe'
+CONVERT_CMD = '/usr/bin/convert'
+IDENTIFY_CMD = '/usr/bin/identify'
+MENCODER_CMD = '/usr/bin/mencoder'
 
-OVERLAY_CENTER = "(W-w)/2:(H-h)/2"
-OVERLAY_BOTTOM_LEFT = "0:H-h"
-OVERLAY_BOTTOM_RIGHT = "W-w:H-h"
-OVERLAY_TOP_LEFT = "0:0"
-OVERLAY_TOP_RIGHT = "W-w:0"
+OVERLAY_CENTER = '(W-w)/2:(H-h)/2'
+OVERLAY_BOTTOM_LEFT = '0:H-h'
+OVERLAY_BOTTOM_RIGHT = 'W-w:H-h'
+OVERLAY_TOP_LEFT = '0:0'
+OVERLAY_TOP_RIGHT = 'W-w:0'
 
 def create_video(image, video, length, framerate=5, params=''):
     '''Create video from animated gif.
@@ -48,39 +48,30 @@ def create_video(image, video, length, framerate=5, params=''):
 
     # extract images from animated gif
     path, ext = os.path.splitext(image)
-    cmd = "%s %s %%03d%s" % (CONVERT_CMD, image, ext)
+    cmd = '%s %s %%03d%s' % (CONVERT_CMD, image, ext)
 
     p = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
     (stdoutdata, stderrdata) = p.communicate()
 
     if p.returncode:
-        raise Exception("Return code is not null")
+        raise Exception('Return code is not null')
 
-    frames = glob.glob("*%s" % ext)
+    frames = glob.glob('*%s' % ext)
 
     # create video from image
-    cmd = "%s -y -r %d -f image2 -i '%%03d%s' %s video.avi" % (FFMPEG_CMD, framerate, ext, params)
+    cmd = '%s -y -r %d -f image2 -i \'%%03d%s\' %s video.avi' % (FFMPEG_CMD, framerate, ext, params)
 
     p = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
     (stdoutdata, stderrdata) = p.communicate()
 
     if p.returncode:
-        raise Exception("Return code is not null")
+        raise Exception('Return code is not null')
 
     length_video = len(frames) / framerate
 
     # create video
-    files = 'video.avi'
-    for i in xrange(0, length / length_video):
-        files = files + " video.avi"
-
-    cmd = "%s -forceidx -oac copy -ovc copy -o %s %s" % (MENCODER_CMD, video, files)
-
-    p = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
-    (stdoutdata, stderrdata) = p.communicate()
-
-    if p.returncode:
-        raise Exception("Return code is not null")
+    files = ['video.avi'] * (length / length_video)
+    merge_video(files, video)
 
     # delete temprorary data
     os.chdir(cwd)
@@ -107,24 +98,24 @@ def get_image_params(image):
     width = 0
     height = 0
 
-    cmd = "%s %s" % (IDENTIFY_CMD, image)
+    cmd = '%s %s' % (IDENTIFY_CMD, image)
 
     p = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
     (stdoutdata, stderrdata) = p.communicate()
 
     if p.returncode:
-        raise Exception("Return code is not null")
+        raise Exception('Return code is not null')
 
     lines = stdoutdata.splitlines()
     if lines:
 
         num_frames = len(lines)
-        width_height_regexp = re.compile(".*\s(?P<width>\d+)x(?P<height>\d+)\s.*")
+        width_height_regexp = re.compile('.*\s(?P<width>\d+)x(?P<height>\d+)\s.*')
         match = width_height_regexp.match(lines[0])
 
         if match:
-            width = int(match.groupdict()["width"])
-            height = int(match.groupdict()["height"])
+            width = int(match.groupdict()['width'])
+            height = int(match.groupdict()['height'])
 
     return (num_frames, width, height)
 
@@ -145,34 +136,34 @@ def get_video_params(video):
     height = 0
     width = 0
 
-    cmd = "%s %s" % (FFPROBE_CMD, video)
+    cmd = '%s %s' % (FFPROBE_CMD, video)
 
     p = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
     (stdoutdata, stderrdata) = p.communicate()
 
     if p.returncode:
-        raise Exception("Return code is not null")
+        raise Exception('Return code is not null')
 
-    duration_regexp = re.compile(".*Duration:\s(?P<hours>[0-9.]+):(?P<minutes>[0-9.]+):(?P<seconds>[0-9.]+),.*")
-    width_height_regexp = re.compile(".*Stream.*,\s(?P<width>\d+)x(?P<height>\d+).*")
+    duration_regexp = re.compile('.*Duration:\s(?P<hours>[0-9.]+):(?P<minutes>[0-9.]+):(?P<seconds>[0-9.]+),.*')
+    width_height_regexp = re.compile('.*Stream.*,\s(?P<width>\d+)x(?P<height>\d+).*')
 
     for line in stderrdata.splitlines():
 
         match = duration_regexp.match(line)
         if not length and match:
-            seconds = int(round(float(match.groupdict()["seconds"])))
-            minutes = int(round(float(match.groupdict()["minutes"])))
-            hours = int(round(float(match.groupdict()["hours"])))
+            seconds = int(round(float(match.groupdict()['seconds'])))
+            minutes = int(round(float(match.groupdict()['minutes'])))
+            hours = int(round(float(match.groupdict()['hours'])))
             length = seconds + 60 * minutes + 3600 * hours
 
         match = width_height_regexp.match(line)
         if not height and not width and match:
-            width = int(match.groupdict()["width"])
-            height = int(match.groupdict()["height"])
+            width = int(match.groupdict()['width'])
+            height = int(match.groupdict()['height'])
 
     return (length, width, height)
 
-def convert_video(video, extension="mp4", params=''):
+def convert_video(video, extension='mp4', params=''):
     '''Convert video.
 
     Arguments:
@@ -188,21 +179,21 @@ def convert_video(video, extension="mp4", params=''):
     '''
 
     if not os.path.exists(video):
-        raise Exception("No such file %s" % video)
+        raise Exception('No such file %s' % video)
 
-    path = "%s.%s" % (os.path.splitext(video)[0], extension)
-    cmd =  "%s -y -i %s %s %s" % (FFMPEG_CMD, video, params, path)
+    path = '%s.%s' % (os.path.splitext(video)[0], extension)
+    cmd =  '%s -y -sameq -i %s %s %s' % (FFMPEG_CMD, video, params, path)
 
     p = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
     (stdoutdata, stderrdata) = p.communicate()
 
     if p.returncode:
-        raise Exception("Return code is not null")
+        raise Exception('Return code is not null')
 
     return path
 
 def create_overlay_video(video, overlay, new_video, audio=None, overlay_params=OVERLAY_CENTER,
-    video_params="-strict experimental"):
+    video_params='-strict experimental'):
     '''Create video overlay.
     
     Arguments:
@@ -225,28 +216,28 @@ def create_overlay_video(video, overlay, new_video, audio=None, overlay_params=O
     '''
 
     if not os.path.exists(video):
-        raise IOError("No such file %s" % video)
+        raise IOError('No such file %s' % video)
     if not os.path.exists(overlay):
-        raise IOError("No such file %s" % overlay)
-    if not os.path.exists(audio):
-        raise IOError("No such file %s" % audio)
+        raise IOError('No such file %s' % overlay)
+    if audio and not os.path.exists(audio):
+        raise IOError('No such file %s' % audio)
 
     if audio:
         video_length, video_width, video_height = get_video_params(video)
-        cmd_fmt = "%s -y -i %s -t %d -i %s -vf \"movie=%s [logo]; [in][logo] overlay=%s [out]\" %s %s"
+        cmd_fmt = '%s -y -i %s -t %d -i %s -vf \'movie=%s [logo]; [in][logo] overlay=%s [out]\' %s %s'
         cmd = cmd_fmt % (FFMPEG_CMD, audio, video_length, video, overlay,
             overlay_params, video_params, new_video)
     else:
-        cmd_fmt = "%s -y -i %s -vf \"movie=%s [logo]; [in][logo] overlay=%s [out]\" %s %s"
+        cmd_fmt = '%s -y -sameq -i %s -vf \'movie=%s [logo]; [in][logo] overlay=%s [out]\' %s %s'
         cmd = cmd_fmt % (FFMPEG_CMD, video, overlay, overlay_params, video_params, new_video)
 
     p = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
     (stdoutdata, stderrdata) = p.communicate()
 
     if p.returncode:
-        raise Exception("Return code is not null")
+        raise Exception('Return code is not null')
 
-def set_video_hue_and_saturation(video, new_video, hue=0, saturation=1, video_params="-strict experimental"):
+def set_video_hue_and_saturation(video, new_video, hue=0, saturation=1, video_params='-strict experimental'):
     '''Set video hue and saturation.
 
     Arguments:
@@ -263,15 +254,15 @@ def set_video_hue_and_saturation(video, new_video, hue=0, saturation=1, video_pa
 
     '''
 
-    cmd = "%s -y -i %s -vf 'mp=hue=%d:%d' %s %s" % (FFMPEG_CMD, video, hue, saturation, video_params, new_video)
+    cmd = '%s -y -sameq -i %s -vf \'mp=hue=%d:%d\' %s %s' % (FFMPEG_CMD, video, hue, saturation, video_params, new_video)
 
     p = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
     (stdoutdata, stderrdata) = p.communicate()
 
     if p.returncode:
-        raise Exception("Return code is not null")
+        raise Exception('Return code is not null')
 
-def set_video_brightness_and_contrast(video, new_video, brightness=0, contrast=0, video_params="-strict experimental"):
+def set_video_brightness_and_contrast(video, new_video, brightness=0, contrast=0, video_params='-strict experimental'):
     '''Set video brightness and contrast.
 
     Arguments:
@@ -288,17 +279,57 @@ def set_video_brightness_and_contrast(video, new_video, brightness=0, contrast=0
 
     '''
 
-    cmd = "%s -y -i %s -vf 'mp=eq=%d:%d' %s %s" % (FFMPEG_CMD, video, brightness, contrast, video_params, new_video)
+    cmd = '%s -y -sameq -i %s -vf \'mp=eq=%d:%d\' %s %s' % (FFMPEG_CMD, video, brightness, contrast, video_params, new_video)
 
     p = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
     (stdoutdata, stderrdata) = p.communicate()
 
     if p.returncode:
-        raise Exception("Return code is not null")
+        raise Exception('Return code is not null')
+
+def split_video(video, parts, template='_part', video_params=''):
+    '''Split video on parts '''
+    video_parts = []
+    video_length, video_width, video_height = get_video_params(video)
+    root, ext = os.path.splitext(video)
+    cmd_tmpl = '%s -y -sameq -ss %d -t %d -i %s %s %s'
+    i = 0
+
+    for start_pos, stop_pos in parts:
+
+        if start_pos > stop_pos or stop_pos > video_length:
+            raise ValueError('wrong parts parameter')
+
+        i += 1
+        video_part = '%s%s%d%s' % (root, template, i, ext)
+        video_parts.append(video_part)
+
+        cmd = cmd_tmpl % (FFMPEG_CMD, start_pos, stop_pos, video, video_params, video_part)
+
+        p = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+        (stdoutdata, stderrdata) = p.communicate()
+
+        if p.returncode:
+            raise Exception('Return code is not null')
+
+    return video_parts
+
+def merge_video(videos, new_video):
+    '''Merge videos into new video '''
+
+    if videos:
+
+        cmd = '%s -forceidx -oac copy -ovc copy -o %s %s' % (MENCODER_CMD, new_video, ' '.join(videos))
+
+        p = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+        (stdoutdata, stderrdata) = p.communicate()
+
+        if p.returncode:
+            raise Exception('Return code is not null')
 
 def overlay_video_worker(video, overlays, new_video, overlay_params=OVERLAY_CENTER, video_params=''):
     '''Complex overlay video.
-    
+
     Arguments:
 
     Returns:
@@ -347,66 +378,66 @@ def main(argv):
     '''
     from optparse import OptionParser
 
-    usage = "usage: %prog -i IMAGE [-f FRAMERATE] [-a AUDIO] [-o output_video] [--overlay-ceter | ...] <input_video>"
+    usage = 'usage: %prog -i IMAGE [-f FRAMERATE] [-a AUDIO] [-o output_video] [--overlay-ceter | ...] <input_video>'
     parser = OptionParser(usage)
 
-    parser.add_option("--overlay-center",
-        action="store_true",
-        dest="overlay_center",
+    parser.add_option('--overlay-center',
+        action='store_true',
+        dest='overlay_center',
         default=False,
-        help="overlay at center of input video")
+        help='overlay at center of input video')
 
-    parser.add_option("--overlay-bottom-left",
-        action="store_true",
-        dest="overlay_bottom_left",
+    parser.add_option('--overlay-bottom-left',
+        action='store_true',
+        dest='overlay_bottom_left',
         default=False,
-        help="overlay at bottom left of input video")
+        help='overlay at bottom left of input video')
 
-    parser.add_option("--overlay-bottom-right",
-        action="store_true",
-        dest="overlay_bottom_right",
+    parser.add_option('--overlay-bottom-right',
+        action='store_true',
+        dest='overlay_bottom_right',
         default=False,
-        help="overlay at bottom right of input video")
+        help='overlay at bottom right of input video')
 
-    parser.add_option("--overlay-top-left",
-        action="store_true",
-        dest="overlay_top_left",
+    parser.add_option('--overlay-top-left',
+        action='store_true',
+        dest='overlay_top_left',
         default=False,
-        help="overlay at top left of input video")
+        help='overlay at top left of input video')
 
-    parser.add_option("--overlay-top-right",
-        action="store_true",
-        dest="overlay_top_right",
+    parser.add_option('--overlay-top-right',
+        action='store_true',
+        dest='overlay_top_right',
         default=False,
-        help="overlay at top right of input video")
+        help='overlay at top right of input video')
 
-    parser.add_option("-i", "--image",
-        action="store",
-        type="string",
-        dest="image",
-        metavar="IMAGE",
-        help="set overlay IMAGE")
+    parser.add_option('-i', '--image',
+        action='store',
+        type='string',
+        dest='image',
+        metavar='IMAGE',
+        help='set overlay IMAGE')
 
-    parser.add_option("-f", "--framerate",
-        action="store",
-        type="int",
-        dest="framerate",
-        metavar="FRAMERATE",
-        help="set overlay animated FRAMERATE")
+    parser.add_option('-f', '--framerate',
+        action='store',
+        type='int',
+        dest='framerate',
+        metavar='FRAMERATE',
+        help='set overlay animated FRAMERATE')
 
-    parser.add_option("-o", "--output-video",
-        action="store",
-        type="string",
-        dest="output_video",
-        metavar="FILE",
-        help="set output video filename")
+    parser.add_option('-o', '--output-video',
+        action='store',
+        type='string',
+        dest='output_video',
+        metavar='FILE',
+        help='set output video filename')
 
-    parser.add_option("-a", "--audio",
-        action="store",
-        type="string",
-        dest="audio",
-        metavar="FILE",
-        help="set audio file as soundtrack")
+    parser.add_option('-a', '--audio',
+        action='store',
+        type='string',
+        dest='audio',
+        metavar='FILE',
+        help='set audio file as soundtrack')
 
     (options, args) = parser.parse_args()
 
@@ -440,20 +471,20 @@ def main(argv):
             new_video = os.path.abspath(new_video)
     else:
         path, ext = os.path.splitext(video)
-        new_video = "%s_overlay.mp4" % (path)
+        new_video = '%s_overlay.mp4' % (path)
 
     video_length, video_width, video_height = get_video_params(video)
     image_num_frames, image_width, image_height = get_image_params(image)
 
     if not image_num_frames:
-        print >> sys.stderr, "Image is broken!"
+        print >> sys.stderr, 'Image is broken!'
         return 1
 
     if image_num_frames == 1:
         image_video = image
     else:
         image_path, image_ext = os.path.splitext(image)
-        image_video = "%s.mp4" % (image_path)
+        image_video = '%s.mp4' % (image_path)
 
         if options.framerate:
             create_video(image, image_video, video_length, options.framerate)
@@ -470,5 +501,5 @@ def main(argv):
 
     return 0
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit(main(sys.argv))
